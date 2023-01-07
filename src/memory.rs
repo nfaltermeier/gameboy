7dc72f2c755cd751ee1eb6aeb64b266c97fa3b07
nfaltermeier <nefalt2@gmail.com>
2023-01-07 03:09:50 -0600

    Fix 16 bit register order

--- src/memory.rs
+++ src/memory.rs
@@ -11,15 +11,49 @@ bitflags! {
     }
 }
 
+fn u8s_to_u16(h: u8, l: u8) -> u16 {
+    ((h as u16) << 8) | l as u16
+}
+
+fn u16_to_u8s(d: u16) -> (u8, u8) {
+    ((d >> 8) as u8, d as u8)
+}
+
 #[repr(C)]
-pub union RegisterPair {
-    pub comb: u16,
+pub struct RegisterPair {
     pub ind: (u8, u8),
+}
+
+impl RegisterPair {
+    pub fn r16(&self) -> u16 {
+        u8s_to_u16(self.ind.0, self.ind.1)
+    }
+
+    pub fn s16(&mut self, val: u16) {
+        let vals = u16_to_u8s(val);
+        self.ind = vals;
+    }
+
+    pub fn uinc16(&mut self) {
+        let result = self.ind.1.overflowing_add(1);
+        self.ind.1 = result.0;
+        if result.1 {
+            self.ind.0 = self.ind.0.wrapping_add(1);
+        }
+    }
+
+    pub fn udec16(&mut self) {
+        let result = self.ind.1.overflowing_sub(1);
+        self.ind.1 = result.0;
+        if result.1 {
+            self.ind.0 = self.ind.0.wrapping_sub(1);
+        }
+    }
 }
 
 impl Default for RegisterPair {
     fn default() -> Self {
-        RegisterPair { comb: 0 }
+        RegisterPair { ind: (0, 0) }
     }
 }
 
@@ -69,5 +103,64 @@ impl Default for Memory {
             r: Default::default(),
             mem: [0; 0x10000],
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RegisterPair;
+
+    #[test]
+    fn register_pair_uinc_16_1() {
+        let mut r = RegisterPair::default();
+        r.s16(0);
+        r.uinc16();
+
+        assert_eq!(1, r.r16())
+    }
+
+    #[test]
+    fn register_pair_uinc_16_lower_overflow() {
+        let mut r = RegisterPair::default();
+        r.s16(0x00FF);
+        r.uinc16();
+
+        assert_eq!(0x0100, r.r16())
+    }
+
+    #[test]
+    fn register_pair_uinc_16_total_overflow() {
+        let mut r = RegisterPair::default();
+        r.s16(0xFFFF);
+        r.uinc16();
+
+        assert_eq!(0, r.r16())
+    }
+
+    #[test]
+    fn register_pair_udec_16_1() {
+        let mut r = RegisterPair::default();
+        r.s16(1);
+        r.udec16();
+
+        assert_eq!(0, r.r16())
+    }
+
+    #[test]
+    fn register_pair_udec_16_half_underflow() {
+        let mut r = RegisterPair::default();
+        r.s16(0x0100);
+        r.udec16();
+
+        assert_eq!(0x00FF, r.r16())
+    }
+
+    #[test]
+    fn register_pair_udec_16_total_underflow() {
+        let mut r = RegisterPair::default();
+        r.s16(0);
+        r.udec16();
+
+        assert_eq!(0xFFFF, r.r16())
     }
 }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -4,40 +4,36 @@ use crate::memory::Memory;
 use crate::operations::*;
 
 fn get_register_mut(mem: &mut Memory, code: u8) -> &mut u8 {
-    unsafe {
-        match code {
-            0b00000111 => &mut mem.r.a,
-            0 => &mut mem.r.bc.ind.0,
-            0b00000001 => &mut mem.r.bc.ind.1,
-            0b00000010 => &mut mem.r.de.ind.0,
-            0b00000011 => &mut mem.r.de.ind.1,
-            0b00000100 => &mut mem.r.bc.ind.0,
-            0b00000101 => &mut mem.r.bc.ind.1,
-            0b00000110 => mem.mut_8(mem.r.hl.comb),
-            _ => panic!(
-                "Unrecognized register code in get_register_mut: {:#b} (shifted to lsb?)",
-                code
-            ),
-        }
+    match code {
+        0b00000111 => &mut mem.r.a,
+        0 => &mut mem.r.bc.ind.0,
+        0b00000001 => &mut mem.r.bc.ind.1,
+        0b00000010 => &mut mem.r.de.ind.0,
+        0b00000011 => &mut mem.r.de.ind.1,
+        0b00000100 => &mut mem.r.bc.ind.0,
+        0b00000101 => &mut mem.r.bc.ind.1,
+        0b00000110 => mem.mut_8(mem.r.hl.r16()),
+        _ => panic!(
+            "Unrecognized register code in get_register_mut: {:#b} (shifted to lsb?)",
+            code
+        ),
     }
 }
 
 fn get_register_val(mem: &Memory, code: u8) -> u8 {
-    unsafe {
-        match code {
-            0b00000111 => mem.r.a,
-            0 => mem.r.bc.ind.0,
-            0b00000001 => mem.r.bc.ind.1,
-            0b00000010 => mem.r.de.ind.0,
-            0b00000011 => mem.r.de.ind.1,
-            0b00000100 => mem.r.hl.ind.0,
-            0b00000101 => mem.r.hl.ind.1,
-            0b00000110 => mem.read_8(mem.r.hl.comb),
-            _ => panic!(
-                "Unrecognized register code in get_register_val: {:#b} (shifted to lsb?)",
-                code
-            ),
-        }
+    match code {
+        0b00000111 => mem.r.a,
+        0 => mem.r.bc.ind.0,
+        0b00000001 => mem.r.bc.ind.1,
+        0b00000010 => mem.r.de.ind.0,
+        0b00000011 => mem.r.de.ind.1,
+        0b00000100 => mem.r.hl.ind.0,
+        0b00000101 => mem.r.hl.ind.1,
+        0b00000110 => mem.read_8(mem.r.hl.r16()),
+        _ => panic!(
+            "Unrecognized register code in get_register_val: {:#b} (shifted to lsb?)",
+            code
+        ),
     }
 }
 
@@ -72,88 +68,76 @@ pub fn process_instruction(mem: &mut Memory) {
         }
         "00_001_010" => {
             // LD A (BC)
-            unsafe {
-                mem.r.a = mem.read_8(mem.r.bc.comb);
-            }
+            mem.r.a = mem.read_8(mem.r.bc.r16());
             cycles += 1;
         }
         "00_011_010" => {
             // LD A (DE)
-            unsafe {
-                mem.r.a = mem.read_8(mem.r.de.comb);
-            }
+            mem.r.a = mem.read_8(mem.r.de.r16());
             cycles += 1;
         }
         "00_101_010" => {
             // LD A (HLI)
-            unsafe {
-                mem.r.a = mem.read_8(mem.r.hl.comb);
-                mem.r.hl.comb += 1;
-            }
+            mem.r.a = mem.read_8(mem.r.hl.r16());
+            mem.r.hl.uinc16();
             cycles += 1;
         }
         "00_111_010" => {
             // LD A (HLD)
-            unsafe {
-                mem.r.a = mem.read_8(mem.r.hl.comb);
-                mem.r.hl.comb -= 1;
-            }
+            mem.r.a = mem.read_8(mem.r.hl.r16());
+            mem.r.hl.udec16();
             cycles += 1;
         }
         "00_000_010" => {
             // LD (BC) A
-            unsafe {
-                *mem.mut_8(mem.r.bc.comb) = mem.r.a;
-            }
+            *mem.mut_8(mem.r.bc.r16()) = mem.r.a;
             cycles += 1;
         }
         "00_010_010" => {
             // LD (DE) A
-            unsafe {
-                *mem.mut_8(mem.r.de.comb) = mem.r.a;
-            }
+            *mem.mut_8(mem.r.de.r16()) = mem.r.a;
             cycles += 1;
         }
         "00_100_010" => {
             // LD (HLI) A
-            unsafe {
-                *mem.mut_8(mem.r.hl.comb) = mem.r.a;
-                mem.r.hl.comb += 1;
-            }
+            *mem.mut_8(mem.r.hl.r16()) = mem.r.a;
+            mem.r.hl.uinc16();
             cycles += 1;
         }
         "00_110_010" => {
             // LD (HLD) A
-            unsafe {
-                *mem.mut_8(mem.r.hl.comb) = mem.r.a;
-                mem.r.hl.comb -= 1;
-            }
+            *mem.mut_8(mem.r.hl.r16()) = mem.r.a;
+            mem.r.hl.udec16();
             cycles += 1;
         }
         "00_dd0_001" => {
             // LD dd nn
             let val = u8s_to_u16(mem.read_8(mem.r.pc + 1), mem.read_8(mem.r.pc));
-            let to = unsafe {
-                match d {
-                    0 => &mut mem.r.bc.comb,
-                    0b00000001 => &mut mem.r.de.comb,
-                    0b00000010 => &mut mem.r.hl.comb,
-                    0b00000011 => &mut mem.r.sp,
-                    _ => panic!("Unknown load code in LD dd nn: {:#b}", d),
+            match d {
+                0 => {
+                    mem.r.bc.s16(val);
                 }
+                0b00000001 => {
+                    mem.r.de.s16(val);
+                }
+                0b00000010 => {
+                    mem.r.hl.s16(val);
+                }
+                0b00000011 => mem.r.sp = val,
+                _ => panic!("Unknown load code in LD dd nn: {:#b}", d),
             };
-            *to = val;
             cycles += 2;
-        },
+        }
         "00_001_000" => {
             // LD (nn), SP
             let vals = u16_to_u8s(mem.r.sp);
             cycles += 4;
-        },
+            unimplemented!();
+        }
         "01_110_110" => {
             // HALT
             todo!();
-        },
+        }
         "01mmmlll" => {
             // LD r r'
             let from_val = get_register_val(mem, l);
@@ -172,16 +156,12 @@ pub fn process_instruction(mem: &mut Memory) {
         }
         "11_110_010" => {
             // LD A (C)
-            unsafe {
-                mem.r.a = mem.read_8(0xFF00 + mem.r.bc.ind.1 as u16);
-            }
+            mem.r.a = mem.read_8(0xFF00 + mem.r.bc.ind.1 as u16);
             cycles += 1;
         }
         "11_100_010" => {
             // LD (C) A
-            unsafe {
-                *mem.mut_8(0xFF00 + mem.r.bc.ind.1 as u16) = mem.r.a;
-            }
+            *mem.mut_8(0xFF00 + mem.r.bc.ind.1 as u16) = mem.r.a;
             cycles += 1;
         }
         "11_110_000" => {
@@ -213,27 +193,23 @@ pub fn process_instruction(mem: &mut Memory) {
         }
         "11_111_001" => {
             // LD SP HL
-            unsafe {
-                mem.r.sp = mem.r.hl.comb;
-            }
+            mem.r.sp = mem.r.hl.r16();
             cycles += 1;
-        },
+        }
         "11_qq0_101" => {
             // PUSH qq
-            let vals = unsafe {
-                match q {
-                    0 => mem.r.bc.ind,
-                    0b00000001 => mem.r.de.ind,
-                    0b00000010 => mem.r.hl.ind,
-                    0b00000011 => (mem.r.a, mem.r.f.bits()),
-                    _ => panic!("Unknown register code in PUSH: {}", q)
-                }
+            let vals = match q {
+                0 => mem.r.bc.ind,
+                0b00000001 => mem.r.de.ind,
+                0b00000010 => mem.r.hl.ind,
+                0b00000011 => (mem.r.a, mem.r.f.bits()),
+                _ => panic!("Unknown register code in PUSH: {}", q),
             };
             mem.write_8(mem.r.sp - 1, vals.0);
             mem.write_8(mem.r.sp - 2, vals.1);
             mem.r.sp -= 2;
             cycles += 3;
-        },
+        }
         "11_qq0_001" => {
             // POP qq
             let vals = (mem.read_8(mem.r.sp + 1), mem.read_8(mem.r.sp));
@@ -241,26 +217,25 @@ pub fn process_instruction(mem: &mut Memory) {
                 mem.r.a = vals.0;
                 mem.r.set_flags_unchecked(vals.1);
             } else {
-                let ptrs = unsafe {
-                    match q {
-                        0 => &mut mem.r.bc.ind,
-                        0b00000001 => &mut mem.r.de.ind,
-                        0b00000010 => &mut mem.r.hl.ind,
-                        _ => panic!("Unknown register code in POP: {}", q)
-                    }
+                let ptrs = match q {
+                    0 => &mut mem.r.bc.ind,
+                    0b00000001 => &mut mem.r.de.ind,
+                    0b00000010 => &mut mem.r.hl.ind,
+                    _ => panic!("Unknown register code in POP: {}", q),
                 };
                 *ptrs = vals;
             }
             mem.r.sp += 2;
             cycles += 2;
-        },
+        }
         "11_111_000" => {
             // LDHL SP, e
             let e = mem.read_8(mem.r.pc) as i8;
             mem.r.pc += 1;
-            mem.r.hl.comb = add_16_mixed(mem.r.sp, e, mem);
+            let result = add_16_mixed(mem.r.sp, e, mem);
+            mem.r.hl.s16(result);
             cycles += 2;
-        },
+        }
         _ => todo!(),
     }
 
@@ -278,7 +253,7 @@ mod tests {
         let mut m = Memory::default();
         let initial_bc = 0xDEAD;
 
-        m.r.bc.comb = initial_bc;
+        m.r.bc.s16(initial_bc);
         m.r.pc = 0x8000;
         m.r.sp = 0x9000;
         // PUSH bc
@@ -286,11 +261,13 @@ mod tests {
         // POP bc
         m.write_8(0x8001, 0b11000001);
         process_instruction(&mut m);
-        m.r.bc.comb = 0;
+        m.r.bc.s16(0);
         process_instruction(&mut m);
-        unsafe {
-            assert_eq!(m.r.bc.comb, initial_bc, "PUSHing and then POPing changes the pushed value");
-        }
+        assert_eq!(
+            m.r.bc.r16(),
+            initial_bc,
+            "PUSHing and then POPing changes the pushed value"
+        );
     }
 
     #[test]
@@ -305,10 +282,8 @@ mod tests {
         m.write_8(0x8000, 0b11_000_001);
         process_instruction(&mut m);
 
-        unsafe {
-            assert_eq!(m.r.bc.ind.0, 0x3C);
-            assert_eq!(m.r.bc.ind.1, 0x5F);
-        }
+        assert_eq!(m.r.bc.ind.0, 0x3C);
+        assert_eq!(m.r.bc.ind.1, 0x5F);
     }
 
     #[test]
@@ -320,10 +295,8 @@ mod tests {
         m.write_8(0x8001, 0x5B);
         m.write_8(0x8002, 0x3A);
         process_instruction(&mut m);
-        unsafe {
-            assert_eq!(m.r.hl.comb, 0x3A5B);
-            assert_eq!(m.r.hl.ind.0, 0x3A);
-            assert_eq!(m.r.hl.ind.1, 0x5B);
-        }
+        assert_eq!(m.r.hl.r16(), 0x3A5B);
+        assert_eq!(m.r.hl.ind.0, 0x3A);
+        assert_eq!(m.r.hl.ind.1, 0x5B);
     }
 }
